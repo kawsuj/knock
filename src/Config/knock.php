2b2fb7423b2c10f333db604e1a432d9cd1c1fdb0
kawsu Jawara <kawsuj@gmail.com>
2016-08-28 15:08:18 +0100

    Relative Image urls

--- src/Config/knock.php
+++ src/Config/knock.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-	
-		'applogo' => env('APP_LOGO_URI','/images/knock_logo.png'),
-		'knocklogo' => env('KNOCK_LOGO_URI','/images/knock_logo.png'),
-		'appfavicon' => env('APP_FAVICON','/knock_favicon.png?v=2'),
-		'knockfavicon' => env('KNOCK_FAVICON','/knock_favicon.png?v=2'),
-		'apphomepage' => env('APP_HOME_PAGE','/'),
-];
+		'applogo' => env('APP_LOGO_URI', asset('/images/knock_logo.png')),
+		'knocklogo' => env('KNOCK_LOGO_URI', asset('/images/knock_logo.png')),
+		'appfavicon' => env('APP_FAVICON', asset('/knock_favicon.png?v=2')),
+		'knockfavicon' => env('KNOCK_FAVICON', asset('/knock_favicon.png?v=2')),
+		'apphomepage' => env('APP_HOME_PAGE', asset('/')),
+		];
