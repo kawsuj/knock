6f1e9b863f804cfd4873c2038d660c2c4ce134b0
kawsu Jawara <kawsuj@gmail.com>
2016-08-30 09:01:51 +0100

    bug fix: image urls

--- src/Config/knock.php
+++ src/Config/knock.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-		'applogo' => env('APP_LOGO_URI', asset('/images/knock_logo.png')),
+	
+		'applogo' => env('APP_LOGO_URI',asset('/images/knock_logo.png')),
 		'knocklogo' => env('KNOCK_LOGO_URI', asset('/images/knock_logo.png')),
 		'appfavicon' => env('APP_FAVICON', asset('/knock_favicon.png?v=2')),
 		'knockfavicon' => env('KNOCK_FAVICON', asset('/knock_favicon.png?v=2')),
 		'apphomepage' => env('APP_HOME_PAGE', asset('/')),
-		];
+];
